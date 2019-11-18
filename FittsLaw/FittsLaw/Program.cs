e18f4605333c0198760662ef2f28d64324c0104c
virtualgirl6 <vicky.bosch@hotmail.com>
2019-11-18 18:08:57 +0100

    Create Program.cs

---
+++ FittsLaw/FittsLaw/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Fitts
+{
+    class Fitts : Form
+    {
+        public Fitts()
+        {
+
+            this.Text = "Fitts' Law";
+            this.BackColor = Color.White;
+            this.WindowState = FormWindowState.Maximized;
+
+            Button startknop = new Button();
+            Button reactieknop = new Button();
+
+            this.Controls.Add(startknop);
+            this.Controls.Add(reactieknop);
+        }
+
+    }
+
+
+
+    class MainClass
+    {
+
+        public static void Main()
+        {
+
+            Fitts scherm;
+            scherm = new Fitts();
+            Application.Run(scherm);
+        }
+    }
+}
