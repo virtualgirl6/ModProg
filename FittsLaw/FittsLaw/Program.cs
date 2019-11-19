e36145eb3590df6b069c6c7b89e834a35bb671c0
virtualgirl6 <vicky.bosch@hotmail.com>
2019-11-19 16:58:11 +0100

    Update Program.cs

--- FittsLaw/FittsLaw/Program.cs
+++ FittsLaw/FittsLaw/Program.cs
@@ -16,7 +16,7 @@ namespace Fitts
             this.Text = "Fitts' Law";
             this.BackColor = Color.White;
             this.WindowState = FormWindowState.Maximized;
-            this.SizeChanged += new EventHandler(SchermMaxEventHandler);
+            this.SizeChanged += new EventHandler(SchermEventHandler);
 
 
             
@@ -32,26 +32,30 @@ namespace Fitts
         {
             startknop.Hide();
             reactieknop.Show();
-
         }
+
 
         void ReactieKnopClick(Object obj, EventArgs e)
         {
             reactieknop.Hide();
             startknop.Show();
-
         }
 
 
-        void SchermMaxEventHandler(object obj, EventArgs e)
+
+        void SchermEventHandler(object obj, EventArgs e)
         {
             int breedte = this.Width; int hoogte = this.Height;
             int startgrootte = 50;
 
             startknop.Size = new Size(startgrootte, startgrootte);
             startknop.Location = new Point(breedte / 2 - (startgrootte / 2), hoogte / 2 - (startgrootte / 2));
+            //startknop text / kleur toevoegen
+
             startknop.Click += new EventHandler(StartKnopClick);
             reactieknop.Click += new EventHandler(ReactieKnopClick);
+
+
             this.Controls.Add(startknop);
             reactieknop.Hide();
         }
@@ -65,9 +69,7 @@ namespace Fitts
 
         public static void Main()
         {
-
-            Fitts scherm;
-            scherm = new Fitts();
+            Fitts scherm = new Fitts();
             Application.Run(scherm);
         }
     }
