b1e161ac8781d50deb777dc65f3ddd7dbf6931e9
virtualgirl6 <vicky.bosch@hotmail.com>
2019-11-21 19:31:29 +0100

    Update Program.cs

--- FittsLaw/FittsLaw/Program.cs
+++ FittsLaw/FittsLaw/Program.cs
@@ -2,67 +2,87 @@
 using System.Drawing;
 using System.Windows.Forms;
 
-namespace Fitts
+namespace FittsLaw
 {
     class Fitts : Form
     {
-        Button startknop = new Button();
-        Button reactieknop = new Button();
-        Random randomgetal = new Random();
 
+        Button startKnop = new Button();
+        Button reactieKnop = new Button();
+        Random randomGetal = new Random();
         long begintijd = new long();
         long totaaltijd = new long();
-        
+
+
         public Fitts()
         {
             this.Text = "Fitts' Law";
             this.BackColor = Color.White;
-            
-        
             this.WindowState = FormWindowState.Maximized;
             this.SizeChanged += SchermStart;
 
+            this.Controls.Add(startKnop);
+            startKnop.Show();
 
-            this.Controls.Add(startknop);
-            startknop.Show();
+            this.Controls.Add(reactieKnop);
+            reactieKnop.Hide();
+        }
 
-            this.Controls.Add(reactieknop);
-            reactieknop.Hide();
 
+        void SchermStart(object obj, EventArgs e)
+        {
+            //formaat van gemaximaliseerd scherm ophalen, om startKnop in het midden te kunnen plaatsen
+            int breedte = this.Width;
+            int hoogte = this.Height;   
+
+            //aanmaken van de startknop
+            int startGrootte = 50;
+            startKnop.Size = new Size(startGrootte, startGrootte);
+            startKnop.Location = new Point(breedte / 2 - (startGrootte / 2), hoogte / 2 - (startGrootte / 2));
+            startKnop.BackColor = Color.DarkGreen;
+            startKnop.Text = "Start";
+
+
+            //"abboneren" op de clickevents 
+            startKnop.Click += StartKnopKlik;
+            reactieKnop.Click += ReactieKnopKlik;
         }
 
 
         void MaakReactieknop()
         {
-            /* met behulp van de random functie wordt het formaat en de locatie van de reactieknop opnieuw bepaald
-             * We hebben hoe groot en klein de knop kan worden ingeperkt. 
-            Om te zorgen dat de knop niet buiten het scherm valt is trekken we de grootte van de knop af van de hoogte/breedte van het scherm */
+            /* Met behulp van de randomfunctie wordt het formaat en de locatie van de reactieknop telkens
+              opnieuw bepaald als deze methode gebruikt wordt. Om (te) moeilijke testen te voorkomen en absurde
+              groottes te voorkokmen, is de randomfunctie die de grootte van de knop aanmaakt ingeperkt. 
+              Daarbij zorgen we dat de knop niet buiten het scherm kan vallen, door de grootte van de knop van
+              de hoogte/breedte van het scherm af te trekken. */
 
-            int randomGrootte = randomgetal.Next(20, this.Width / 8);
-            int randomBreedte = randomgetal.Next(0, this.Width - randomGrootte);
-            int randomHoogte = randomgetal.Next(0, this.Height - randomGrootte);
+            
+            int randomGrootte = randomGetal.Next(20, this.Width / 8);
+            int randomBreedte = randomGetal.Next(0, this.Width - randomGrootte);
+            int randomHoogte = randomGetal.Next(0, this.Height - randomGrootte);
 
 
-            reactieknop.Location = new Point(randomBreedte, randomHoogte);
-            reactieknop.Size = new Size(randomGrootte, randomGrootte);
-            reactieknop.BackColor = Color.Red; 
+            reactieKnop.Location = new Point(randomBreedte, randomHoogte);
+            reactieKnop.Size = new Size(randomGrootte, randomGrootte);
+            reactieKnop.BackColor = Color.Red; 
         }
 
 
-        void StartKnopClick(Object obj, EventArgs e)
+        void StartKnopKlik(Object obj, EventArgs e)
         {
-            startknop.Hide();
+            startKnop.Hide();
             MaakReactieknop();
-            reactieknop.Show();
+            reactieKnop.Show();
             StartTijd();
         }
 
 
-        void ReactieKnopClick(Object obj, EventArgs e)
+        void ReactieKnopKlik(Object obj, EventArgs e)
         {
             EindTijd();
-            reactieknop.Hide();
-            startknop.Show();
+            reactieKnop.Hide();
+            startKnop.Show();
 
             Bereken();
         }
@@ -80,57 +100,34 @@ namespace Fitts
             return totaaltijd;
         }
 
+
         void Bereken()
         {
             /* 
             In deze methode genereren we de data waarmee we Fitts' Law kunnen aantonen (of weerleggen).
-            Aan de hand van de afstanden tussen de start- en reactieknop, en de breedte van de reactieknop,
-            berekenen we de Difficulty Index (DI). Alle data (reactietijd, DI, D en W) worden naar de Console geschreven,
-            zodat we deze naar Excel kunnen exporteren. 
-             */
+            Aan de hand van de afstand tussen de start- en reactieknop (D) en de breedte van de reactieknop (W),
+            berekenen we de Difficulty Index (DI). Alle data (reactietijd T, DI, D en W) worden naar de Console geschreven,
+            zodat we deze naar Excel kunnen exporteren. */
 
-            int W = reactieknop.Width;
+            int W = reactieKnop.Width;
 
-            int Xafstand = Math.Abs(startknop.Location.X - reactieknop.Location.X);     
-            int Yafstand = Math.Abs(startknop.Location.Y - reactieknop.Location.Y);
-            double D = Math.Sqrt(Xafstand * Xafstand + Yafstand * Yafstand);           // pythagoras om de afstand te berekenen
+            // adhv pythagoras de afstand tussen de knoppen berekenen
+            int xAfstand = startKnop.Location.X - reactieKnop.Location.X;     
+            int yAfstand = startKnop.Location.Y - reactieKnop.Location.Y;
+            double D = Math.Sqrt(xAfstand * xAfstand + yAfstand * yAfstand);
 
-            double DI = Math.Log(D / W + 1, 2);                                        // DI = log2(1+D/W)
-            
+            double DI = Math.Log(D / W + 1, 2);                            
+
             Console.WriteLine("T: " + totaaltijd + "\t DI: " + DI + "\t D: " + D + "\t W: " + W);
         }
-
-
-
-        void SchermStart(object obj, EventArgs e)
-        {
-            
-            //aanmaken van de startknop
-
-            int breedte = this.Width; int hoogte = this.Height;    //formaat van gemaximaliseerd scherm ophalen
-            int startgrootte = 50;
-
-            startknop.Size = new Size(startgrootte, startgrootte);
-            startknop.Location = new Point(breedte / 2 - (startgrootte / 2), hoogte / 2 - (startgrootte / 2));
-            startknop.BackColor = Color.DarkGreen;
-            startknop.Text = "Start";
-            
-
-            //klikevents ????
-            startknop.Click += StartKnopClick;
-            reactieknop.Click += ReactieKnopClick;
-        }
-        
     }
-
 
 
     class MainClass
     {
         public static void Main()
         {
-            Fitts scherm = new Fitts();
-            Application.Run(scherm);
+            Application.Run(new Fitts());
         }
     }
 }
