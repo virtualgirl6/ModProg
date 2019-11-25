3cb0e34038e4db2adec522917f21e5500cd88c00
virtualgirl6 <vicky.bosch@hotmail.com>
2019-11-25 20:31:11 +0100

    mini opzet

---
+++ Mandelbrot/Mandelbrot/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+
+namespace Mandelbrot
+{
+
+    class Mandel : Form
+    {
+
+
+
+    }
+
+
+    class MainClass
+    {
+        static void Main()
+        {
+            Application.Run(new Mandel());
+        }
+    }
+}
