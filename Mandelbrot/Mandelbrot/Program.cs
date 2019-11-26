a64ba272071fd6f1ead19dff4e4cd1c1af152cec
virtualgirl6 <vicky.bosch@hotmail.com>
2019-11-26 15:24:02 +0100

    Update Program.cs

--- Mandelbrot/Mandelbrot/Program.cs
+++ Mandelbrot/Mandelbrot/Program.cs
@@ -8,7 +8,31 @@ namespace Mandelbrot
 
     class Mandel : Form
     {
+        // 4 x textbox + tekst
+        TextBox txtMiddenX = new TextBox();
 
+
+        Button okButton = new Button();
+       
+
+
+
+        public Mandel()
+        {
+            this.Text = "Mandelbrot";
+            this.Size = new Size(400, 400);
+            this.BackColor = Color.AliceBlue;
+
+            int txtHoogte = 20;
+            okButton.Location = new Point(50, 50);
+            okButton.Size = new Size(30, txtHoogte);
+            okButton.Text = "OK";
+
+
+            //eventhandlers
+
+
+        }
 
 
     }
@@ -16,7 +40,7 @@ namespace Mandelbrot
 
     class MainClass
     {
-        static void Main()
+        public static void Main()
         {
             Application.Run(new Mandel());
         }
