519632c3751b3d67eeb53d7e1bde81b3ad8ceb40
virtualgirl6 <vicky.bosch@hotmail.com>
2019-12-08 23:26:56 +0100

    Update Program.cs

--- Reversi/Reversi/Program.cs
+++ Reversi/Reversi/Program.cs
@@ -8,6 +8,12 @@ namespace Reversi
     {
         //declaraties
         int columns, rows;
+        Button helpButton = new Button();
+        Button nieuwSpelButton = new Button();
+        Label aanZetLabel = new Label();
+        Panel bord = new Panel();
+        TextBox rijTxtBox = new TextBox();
+        TextBox kolomTxtBox = new TextBox();
 
         public Reversi() //constructor
         {
@@ -15,12 +21,26 @@ namespace Reversi
             this.Text = "Reversi";
             this.BackColor = Color.DarkGray;
 
-            //knopjes enzo
+            //opmaak knoppen
+            helpButton.Location = new Point(50, 20);
+            helpButton.Size = new Size(60, 35);
+            helpButton.Text = "help";
+            helpButton.BackColor = Color.AliceBlue;
 
-            Panel bord = new Panel();
+            nieuwSpelButton.Location = new Point(125, 20);
+            nieuwSpelButton.Size = new Size(80, 35);
+            nieuwSpelButton.Text = "nieuw spel";
+            nieuwSpelButton.BackColor = Color.AliceBlue;
+
+            //txtboxen
+
+            aanZetLabel.Location = new Point(50, 80);
+            aanZetLabel.Text = "aan zet: xxx ";  //font moet groter!
+
+
             bord.Size = new Size(501, 501); //+1 om de laatste zwarte lijn van bord ook te weergeven
             bord.Location = new Point(50, 150);
-
+            bord.BackColor = Color.GhostWhite;
 
             //beginwaarden
             columns = 10; //min 3, max 10? waarden geven, zie NieuwSpel()
@@ -30,7 +50,11 @@ namespace Reversi
             bord.Paint += TekenForm;
 
             //toevoegen
+            
             Controls.Add(bord);
+            Controls.Add(nieuwSpelButton);
+            Controls.Add(helpButton);
+            Controls.Add(aanZetLabel);
         }
 
 
@@ -41,7 +65,7 @@ namespace Reversi
             for (int x = 0; x < rows; x++)
                 for (int y = 0; y < columns; y++)
                     pea.Graphics.DrawRectangle(p, 50*x, 50*y, 50, 50);
-                    
+          
         }
 
         void KlikVakje(object o, MouseEventArgs mea)
@@ -74,7 +98,6 @@ namespace Reversi
         }
     }
 
-   
 
     class MainClass
     {
