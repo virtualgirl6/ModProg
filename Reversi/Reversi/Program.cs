623dcb26c1a8a1daae9b12b93b3bed57ad4e8b26
virtualgirl6 <vicky.bosch@hotmail.com>
2019-12-08 20:11:08 +0100

    Update Program.cs

--- Reversi/Reversi/Program.cs
+++ Reversi/Reversi/Program.cs
@@ -11,20 +11,26 @@ namespace Reversi
 
         public Reversi() //constructor
         {
-            this.Size = new Size(500, 500);
+            this.Size = new Size(600, 700);
             this.Text = "Reversi";
             this.BackColor = Color.DarkGray;
 
-            //knopjes 
+            //knopjes enzo
+
+            Panel bord = new Panel();
+            bord.Size = new Size(501, 501); //+1 om de laatste zwarte lijn van bord ook te weergeven
+            bord.Location = new Point(50, 150);
 
 
             //beginwaarden
-            columns = 6;
-            rows = 6;
+            columns = 10; //min 3, max 10? waarden geven, zie NieuwSpel()
+            rows = 10;
 
             //eventhandlers
-            this.Paint += TekenForm;
+            bord.Paint += TekenForm;
 
+            //toevoegen
+            Controls.Add(bord);
         }
 
 
@@ -34,7 +40,37 @@ namespace Reversi
             Pen p = Pens.Black;
             for (int x = 0; x < rows; x++)
                 for (int y = 0; y < columns; y++)
-                    pea.Graphics.DrawRectangle(p, (50*x) +100, (50*y) + 100, 50, 50);
+                    pea.Graphics.DrawRectangle(p, 50*x, 50*y, 50, 50);
+                    
+        }
+
+        void KlikVakje(object o, MouseEventArgs mea)
+        {
+            for (int x = 0; x < rows; x++)
+                for (int y = 0; y < columns; y++)
+                    ZetSteen(x, y);
+
+
+        }
+
+        void ZetSteen(int r, int c)
+        {
+            //if blauwbeurt = true
+            //teken blauw rondje op 50x 50y
+            //if roodbeurt = true
+            // 
+        }
+
+        void LegaalCheck() //locatie als parameter?
+        {
+
+
+        }
+
+        void Nieuwspel(object o, MouseEventArgs mea)
+        {
+            //geef waardes mee aan rows en columns. popup textbox?
+            //bord moet dus ook kleiner worden in het begin. (6x6)
         }
     }
 
