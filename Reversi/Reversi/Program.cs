fe838ab806b2412717e9de1f1f991fea315e8e2d
virtualgirl6 <vicky.bosch@hotmail.com>
2019-12-08 18:23:22 +0100

    Create Program.cs

---
+++ Reversi/Reversi/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace Reversi
+{
+    class Reversi : Form
+    {
+        public Reversi()
+        {
+            this.Size = new Size(500, 500);
+            this.Text = "Reversi";
+            this.BackColor = Color.DarkGray;
+
+
+        }
+
+    }
+        
+
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Application.Run(new Reversi());
+        }
+    }
+}
