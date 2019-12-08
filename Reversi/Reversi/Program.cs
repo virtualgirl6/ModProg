1abdb06066cc4313ed3c35f5e1d6484e43984e7b
virtualgirl6 <vicky.bosch@hotmail.com>
2019-12-08 18:39:30 +0100

    Update Program.cs

--- Reversi/Reversi/Program.cs
+++ Reversi/Reversi/Program.cs
@@ -6,21 +6,43 @@ namespace Reversi
 {
     class Reversi : Form
     {
-        public Reversi()
+        //declaraties
+        int columns, rows;
+
+        public Reversi() //constructor
         {
             this.Size = new Size(500, 500);
             this.Text = "Reversi";
             this.BackColor = Color.DarkGray;
 
+            //knopjes 
+
+
+            //beginwaarden
+            columns = 6;
+            rows = 6;
+
+            //eventhandlers
+            this.Paint += TekenForm;
 
         }
 
+
+        void TekenForm(object o, PaintEventArgs pea)
+        {
+            // teken veld 
+            Pen p = Pens.Black;
+            for (int x = 0; x < rows; x++)
+                for (int y = 0; y < columns; y++)
+                    pea.Graphics.DrawRectangle(p, (50*x) +100, (50*y) + 100, 50, 50);
+        }
     }
-        
+
+   
 
     class MainClass
     {
-        public static void Main(string[] args)
+        public static void Main()
         {
             Application.Run(new Reversi());
         }
